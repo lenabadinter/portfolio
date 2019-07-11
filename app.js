aab38e178ee032ee0558abcc626cd3148996e347
Lena Badinter <lena.badinter@gmail.com>
2019-07-11 12:22:13 +0300

    first commit

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var lppModule = (function () {
     }
 
     function _getPlatformType() {
+        return _PlatformTypeEnum.MOBILE;
         const ua = navigator.userAgent.toLowerCase();
         const isSafari = (ua.indexOf('safari') != -1) && (ua.indexOf('chrome') == -1);
         if (isSafari) {
