f0c3871eae178d7ff1e98fa1103aaba92d54ea00
Lena Badinter <lena.badinter@gmail.com>
2021-05-01 10:03:31 +0300

    facebook analytics removed

--- app.js
+++ app.js
@@ -167,32 +167,6 @@ var lppModule = (function() {
         }
       };
       document.getElementsByTagName("head")[0].appendChild(googleAnalytics);
-
-      !(function(f, b, e, v, n, t, s) {
-        if (f.fbq) return;
-        n = f.fbq = function() {
-          n.callMethod
-            ? n.callMethod.apply(n, arguments)
-            : n.queue.push(arguments);
-        };
-        if (!f._fbq) f._fbq = n;
-        n.push = n;
-        n.loaded = !0;
-        n.version = "2.0";
-        n.queue = [];
-        t = b.createElement(e);
-        t.async = !0;
-        t.src = v;
-        s = b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t, s);
-      })(
-        window,
-        document,
-        "script",
-        "https://connect.facebook.net/en_US/fbevents.js"
-      );
-      fbq("init", "413909952648446");
-      fbq("track", "PageView");
     });
 
     document
